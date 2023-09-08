cf4c462e5577a9fda35d3a5c1d98d9c4b9dfb531
smile.tan <intelligentfu@163.com>
2023-09-08 14:33:45 +0800

    support specify nodeport

--- api/doris/v1/types.go
+++ api/doris/v1/types.go
@@ -216,6 +216,9 @@ type ExportService struct {
 	// +optional
 	Type corev1.ServiceType `json:"type,omitempty"`
 
+	//ServicePort config service for NodePort access mode.
+	ServicePorts []DorisServicePort `json:"servicePorts,omitempty"`
+
 	// Only applies to Service Type: LoadBalancer.
 	// This feature depends on whether the underlying cloud-provider supports specifying
 	// the loadBalancerIP when a load balancer is created.
@@ -226,6 +229,33 @@ type ExportService struct {
 	// This field may be removed in a future API version.
 	// +optional
 	LoadBalancerIP string `json:"loadBalancerIP,omitempty"`
+}
+
+// DorisServicePort for ServiceType=NodePort situation.
+type DorisServicePort struct {
+	// The port on each node on which this service is exposed when type is
+	// NodePort or LoadBalancer.  Usually assigned by the system. If a value is
+	// specified, in-range, and not in use it will be used, otherwise the
+	// operation will fail.  If not specified, a port will be allocated if this
+	// Service requires one.  If this field is specified when creating a
+	// Service which does not need it, creation will fail. This field will be
+	// wiped when updating a Service to no longer need it (e.g. changing type
+	// from NodePort to ClusterIP).
+	// More info: https://kubernetes.io/docs/concepts/services-networking/service/#type-nodeport
+	// need in 30000-32767
+	// +optional
+	NodePort int32 `json:"nodePort,omitempty"`
+
+	// Number or name of the port to access on the pods targeted by the service.
+	// Number must be in the range 1 to 65535. Name must be an IANA_SVC_NAME.
+	// If this is a string, it will be looked up as a named port in the
+	// target Pod's container ports. If this is not specified, the value
+	// of the 'port' field is used (an identity map).
+	// This field is ignored for services with clusterIP=None, and should be
+	// omitted or set equal to the 'port' field.
+	// More info: https://kubernetes.io/docs/concepts/services-networking/service/#defining-a-service
+	// +optional
+	TargetPort int32 `json:"targetPort,omitempty"`
 }
 
 // DorisClusterStatus defines the observed state of DorisCluster
