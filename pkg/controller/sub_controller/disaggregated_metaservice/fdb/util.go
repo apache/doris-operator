6827e2efec70dd17a8b03c730b8a3d8fbc29da05
smile.tan <intelligentfu@163.com>
2024-08-08 17:11:48 +0800

    use selectdb image

--- pkg/controller/sub_controller/disaggregated_metaservice/fdb/util.go
+++ pkg/controller/sub_controller/disaggregated_metaservice/fdb/util.go
@@ -20,11 +20,18 @@ package fdb
 import (
 	"errors"
 	"fmt"
+	"github.com/FoundationDB/fdb-kubernetes-operator/api/v1beta2"
+	"k8s.io/klog/v2"
 	"strings"
 )
 
+const (
+	DefaultFDBImage        = "selectdb/foundationdb:7.1.38"
+	DefaultFDBSidecarImage = "selectdb/foundationdb-kubernetes-sidecar:7.1.36-1"
+)
+
 // use ":" as IPS to split image as baseimage and version.
-func imageSplit(image string) (baseImage, version string, err error) {
+func imageSplit(image string) (baseImage, tag string, err error) {
 	isa := strings.Split(image, ":")
 	if len(isa) == 0 {
 		err = errors.New(fmt.Sprintf("the image = %s format is not provided. please reference docker format.", image))
@@ -32,6 +39,44 @@ func imageSplit(image string) (baseImage, version string, err error) {
 	}
 
 	baseImage = isa[0]
-	version = isa[1]
+	tag = isa[1]
 	return
+}
+
+func fdbImageOverride(image string) (v1beta2.ContainerOverrides, error) {
+	if image == "" {
+		image = DefaultFDBImage
+	}
+
+	return newContainerOverride(image)
+}
+
+func newContainerOverride(image string) (v1beta2.ContainerOverrides, error) {
+	if image == "" {
+		return v1beta2.ContainerOverrides{}, errors.New("image is empty")
+	}
+
+	bi, tag, err := imageSplit(image)
+	if err != nil {
+		klog.Infof("disaggregatedFDBController split config image error, err=%s", err.Error())
+		return v1beta2.ContainerOverrides{}, err
+
+	}
+
+	return v1beta2.ContainerOverrides{
+		ImageConfigs: []v1beta2.ImageConfig{
+			v1beta2.ImageConfig{
+				BaseImage: bi,
+				Tag:       tag,
+			},
+		},
+	}, nil
+}
+
+func fdbSidecarImageOverride(image string) (v1beta2.ContainerOverrides, error) {
+	if image == "" {
+		image = DefaultFDBSidecarImage
+	}
+
+	return newContainerOverride(image)
 }
